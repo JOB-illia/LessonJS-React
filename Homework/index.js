7edd869642636e658f2e6127ad20794fc4f5cd66
Illia Klen <klenchys.i@gmail.com>
2020-04-04 01:58:41 +0200

    Homework3

--- Homework/index.js
+++ Homework/index.js
@@ -1,72 +1,88 @@
-let money = +prompt('Ваш бюджет на месяц?', ''),
-    time = prompt('Введите дату в данном формате YYYY-MM-DD', '');
+'use strict';
 
+
+let money, time;
+
+function start() {
+    money = +prompt ("Ваш бюджет на месяц?", "");
+    time = prompt ("Введите дату в формате YYYY-MM-DD", "");
+
+    while (isNaN(money) || money == "" || money == null) {
+        money = +prompt ("Ваш бюджет на месяц?", "");
+    }
+
+}
+start();
 
 
 let appData = {
     budget: money,
     timeData: time,
-    expenses : {},
+    expenses: {},
     optionalExpenses: {},
     income: [],
-    savings: false,
+    savings: true
 };
 
-// let iOne = 0;
-// while(iOne < 2){
-//     let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
-//         b = prompt("Во сколько обойдется?", '');
-//     if((typeof(a))  === 'string' && (typeof (a)) != null && (typeof (b)) != null
-//         && a != '' && b != '' && a.length < 50) {
-//         console.log('done');
-//         appData.expenses[a] = b;
-//     }else {
-//         continue;
-//     }
-//     iOne++;
-// }
-// let iOne = 0;
-// do {
-//     let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
-//         b = prompt("Во сколько обойдется?", '');
-//     if((typeof(a))  === 'string' && (typeof (a)) != null && (typeof (b)) != null
-//         && a != '' && b != '' && a.length < 50) {
-//         console.log('done');
-//         appData.expenses[a] = b;
-//     }else {
-//         continue;
-//     }
-//     iOne++;
-// }while(iOne < 2);
 
-for (let i = 0; i < 2; i++) {
-    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
-        b = prompt("Во сколько обойдется?", '');
-    if((typeof(a))  === 'string' && (typeof (a)) != null && (typeof (b)) != null
-    && a != '' && b != '' && a.length < 50) {
-        console.log('done');
-        appData.expenses[a] = b;
-    }else {
-        continue;
+function chooseExpenses() {
+    for (let i = 0; i < 2; i++) {
+        let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
+            b = prompt ("Во сколько обойдется?", "");
+
+        if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
+            appData.expenses[a] = b;
+        } else {
+            i--;
+        }
+
+    };
+}
+chooseExpenses();
+
+
+function detectDayBudget() {                                            // Расчет дневного бюджета
+    appData.moneyPerDay = (appData.budget / 30).toFixed();
+    alert ("Бюджет на 1 день составляет " + appData.moneyPerDay + "руб.");
+}
+detectDayBudget();
+
+
+function detectLevel() {                                                // Расчет уровня достатка
+    if (appData.moneyPerDay < 100) {
+        console.log ("Это минимальный уровень достатка!");
+    } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+        console.log ("Это средний уровень достатка!");
+    } else if (appData.moneyPerDay > 2000) {
+        console.log ("Это высокий уровень достатка!");
+    } else {
+        console.log ("Ошибочка...!");
     }
 }
+detectLevel();
 
 
-appData.moneyPerDay = appData.budget / 30;
 
-alert('Ежедневный бюджет:' + appData.moneyPerDay);
+function checkSavings() {
+    if (appData.savings == true) {
+        let save = +prompt("Какова сумма накоплений?"),
+            percent = +prompt("Под какой процент?");
 
-if(appData.moneyPerDay < 100 ){
-    console.log('Минимальный уровень достатка');
-}else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000){
-    console.log('Средний уровень достатка');
-}else if (appData.moneyPerDay > 2000){
-    console.log('Высокий уровень достатка');
-}else {
-    console.log('Ошибка - передан не тот тип данных!!!')
+        appData.monthIncome = save/100/12*percent;
+        alert("Доход с Вашего депозита в месяц: " + appData.monthIncome);
+    }
 }
+checkSavings();
 
 
-// 1. Сколько типов данных существует в JS: number / bigint / string / boolean / null / undefined / symbol  = 7
-// 2. Как вывести информацию в консоль?: console.log()
-// 3. Какая функция операторов || и &&?: ИЛИ и И
+function chooseOptExpenses() {                             // Функция для определения необязательных расходов
+
+    for (let i = 1; i <= 3; i++) {
+        let questionOptExpenses = prompt("Статья необязательных расходов?");
+        appData.optionalExpenses[i] = questionOptExpenses;
+        console.log(appData.optionalExpenses);
+    }
+
+
+}
+chooseOptExpenses();
